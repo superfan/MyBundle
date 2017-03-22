8d2438b176a52f5353eee8e086df3f08a8223c52
superfan <bicasi@sina.com>
2017-03-22 22:06:25 +0800

    b

--- mylibrary/src/main/java/com/sh3h/mylibrary/SecondActivity.java
+++ mylibrary/src/main/java/com/sh3h/mylibrary/SecondActivity.java
@@ -2,14 +2,17 @@ package com.sh3h.mylibrary;
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-public class TestActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void onTest6(View view) {
+        Toast.makeText(this, Toast.LENGTH_LONG, "ok").show();
     }
 }
